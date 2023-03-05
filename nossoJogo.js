9762b57e563792b7d4c5af6bec13d55a6fe9430f
LinconCS <linconsantos@gmail.com>
2023-03-04 23:36:08 -0300

    Nosso-Jogo

--- nossoJogo.js
+++ nossoJogo.js
@@ -1,12 +1,33 @@
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
+console.log('Boas vindas ao jogo de BlackJack!')
+
+if(confirm('Quer iniciar uma nova rodada?')) {
+    const cartaUmUsuario = comprarCarta()
+    const cartaDoisUsuario = comprarCarta()
+    const cartaUmComputador = comprarCarta()
+    const cartaDoisComputador = comprarCarta()
     
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
+    const pontuacaoUsuario = cartaUmUsuario.valor + cartaDoisUsuario.valor
+    const pontuacaoComputador = cartaUmComputador.valor + cartaDoisComputador.valor
+    
+    console.log(`Usuário - cartas: ${cartaUmUsuario.texto} ${cartaDoisUsuario.texto} - pontuação ${pontuacaoUsuario}`)
+    console.log(`Computador - cartas: ${cartaUmComputador.texto} ${cartaDoisComputador.texto} - pontuação ${pontuacaoComputador}`)
+    
+    if (pontuacaoUsuario <= 21 && pontuacaoComputador <= 21) {
+        if (pontuacaoUsuario > pontuacaoComputador) {
+            console.log('O usuário ganhou!')
+        } else if (pontuacaoComputador > pontuacaoUsuario) {
+            console.log('O computador ganhou!')
+        } else if (pontuacaoUsuario === pontuacaoComputador)
+            console.log('Empate!')
+        }
+    if (pontuacaoUsuario > 21 || pontuacaoComputador> 21) {
+        if (pontuacaoUsuario > 21)
+            console.log('O usuário estourou, computador ganhou!')
+        } else if (pontuacaoComputador > 21) {
+            console.log('O computador estourou, usuário ganhou!')
+        } else if (pontuacaoUsuario > 21 && pontuacaoComputador > 21) {
+            console.log('Ninguém ganhou!')
+        }
+} else {
+    console.log('O jogo acabou.')
+}
